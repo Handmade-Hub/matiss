854438422ccc5dadc5c985f3245ce10efbaf61b1
Igorek <ehotzvezdolet@gmail.com>
2024-03-12 09:01:19 +0100

    home page without mobile

--- Layout/dist/js/global.js
+++ Layout/dist/js/global.js
@@ -1,13 +1,78 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
  // banner swiper
  if (document.querySelectorAll('.banner__swiper').length) {
   const swiper = new Swiper('.banner__swiper', {
    pagination: {
-    el: '.swiper-pagination',
-  },
+    el: '.banner__swiper-pagination',
+   },
   });
  }
 
+ // new-collection swiper
+ if (document.querySelectorAll('.new-collection__swiper').length) {
+  const swiper = new Swiper('.new-collection__swiper', {
+   spaceBetween: 32,
+   slidesPerView: 3,
+   pagination: {
+    el: '.new-collection__swiper-pagination',
+   },
+  });
+ }
+
+ // goal swiper
+ if (document.querySelectorAll('.goal__swiper').length) {
+  const swiper = new Swiper('.goal__swiper', {
+   navigation: {
+    nextEl: '.goal__swiper-button-next',
+    prevEl: '.goal__swiper-button-prev',
+  },
+   pagination: {
+    el: '.goal__swiper-pagination',
+   },
+  });
+ }
+
+ // clients swiper
+ if (document.querySelectorAll('.clients__swiper').length) {
+  const swiper = new Swiper('.clients__swiper', {
+   loop: true,
+   spaceBetween: 30,
+   slidesPerView: 3,
+   speed: 3000,
+   autoplay: {
+    delay: 0,
+   },
+   breakpoints: {
+    575: {
+     slidesPerView: 4,
+    },
+    768: {
+     slidesPerView: 5,
+    },
+    992: {
+     slidesPerView: 6,
+     spaceBetween: 60,
+    },
+    1200: {
+     slidesPerView: 6.8,
+    }
+   }
+  });
+ }
+
+ // description accordion
+ if (document.querySelectorAll('.description').length) {
+  const wrapper = document.querySelector('.description__wrapper');
+  const button = wrapper.querySelector('.description__button');
+
+  button.addEventListener('click', e => {
+   button.classList.toggle('active');
+   wrapper.classList.toggle('active');
+   if (wrapper.classList.contains('active')) wrapper.style.maxHeight = wrapper.scrollHeight + 'px';
+   else wrapper.style.maxHeight = '360px';
+  });
+
+ }
 
 });
